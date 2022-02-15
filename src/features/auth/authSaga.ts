d0abc02a3fd66466ab93720d172fbd76dc865459
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-02-15 21:54:11 +0700

    update

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -21,7 +21,7 @@ import {
 
 function Login(payload: PayloadAction<LoginPayload>) {
   const apiClient = axios.create({
-    baseURL: "https://orchid-react-app.herokuapp.com",
+    baseURL: "https://orchid.tk/api/v1",
     withCredentials: true,
   });
   const config = { headers: { "Content-Type": "application/json" } };
@@ -69,7 +69,7 @@ function* handleResetPassword(action: PayloadAction<ForgotPaload>) {
 
 const Logout = () => {
   const apiClient = axios.create({
-    baseURL: "https://orchid-react-app.herokuapp.com",
+    baseURL: "https://orchid.tk/api/v1",
     withCredentials: true,
     // credentials: "include",
   });
