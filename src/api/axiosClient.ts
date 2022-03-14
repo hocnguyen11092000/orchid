065c053549a00a0efc8d7428fb39aa3db059338b
Hoc Nguyen <hocnguyen1109200@gmail.com>
2022-03-14 12:41:36 +0700

    change

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "https://orchid/api/v1",
+  baseURL: "https://orchid.tk/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
